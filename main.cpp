bf64705e6afd414edeb417da19ba8478a969deed
Athalie Aurora <119656945+flauuralix@users.noreply.github.com>
2023-04-01 21:20:12 +0700

    Menambahkan TUBES_SDA.dev dan main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*       
+File Name		: main.cpp
+Description		: Tubes Struktur Data dan Algoritma
+Author			: 
+*/
+
+/* ========== Header File ========== */
+
+#include <stdio.h>
+
+/* ======= End of Header File ====== */
+
+
+
+int main()
+{
+	printf("Selamat Datang di supermarket, selamat berbelanja");
+}
