dacc11767f337d019f27a1e1257356aced2632c5
flauuralix <119656945+flauuralix@users.noreply.github.com>
2023-04-12 17:10:49 +0700

    Menambahkan boolean.h

---
+++ boolean.h
@@ -0,0 +1,19 @@
+/*       
+File Name		: boolean.h
+Description		: Tubes Struktur Data dan Algoritma
+Author			: 
+*/
+
+/* ========== Header File ========== */
+
+#include <stdio.h>
+#ifndef boolean_H
+#define boolean_H
+#define True 1
+#define False 0
+#define boolean unsigned char
+#endif
+
+
+/* ======= End of Header File ====== */
+
